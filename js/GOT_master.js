3226c3ffac1c4a18cd707884afa6153f4069d33c
Mallory Coleman <mallorycoleman@Mallorys-Air.fc.ca>
2019-03-11 16:43:51 -0400

    added light box to js

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -3,5 +3,17 @@
 
 	// varible stack
 	// grab the shields at the bottom of the page
+	const shields    = document.querySelectorAll('sigil-container'),
+	      lightBox   = document.querySelector('.lightbox');
+
+	function showLightbox() {
+		lightbox.classList.add('show-Lightbox');
+	}
+
+	function hideLightbox() {
+		lightbox.classList.remove('show-Lightbox');
+	}
+
+	shields.forEach(shield => shield.addEventListener('click', showLightbox));
 
 })();
